4c2ceb75c9707f83b599113ec894c1666cbc3517
matthias <mr3lucciola@gmail.com>
2021-05-12 15:53:03 -0400

    feat: likes and comments start

--- src/components/pages/feed/ShowProfilesThatLikedPost.jsx
+++ src/components/pages/feed/ShowProfilesThatLikedPost.jsx
@@ -1,17 +1,12 @@
 // react
 import React from 'react';
-import { Link } from 'react-router-dom';
 // utils
 import { makeStyles } from '@material-ui/core/styles';
-import { Link as MuiLink } from '@material-ui/core';
-// components
-import ShowProfilesThatLikedPost from './ShowProfilesThatLikedPost';
 // constants
 const useStyles = makeStyles(theme => ({
   root: {
     margin: `0 2px`,
     flex: 1,
-    minHeight: 15,
   },
 }));
 
@@ -26,26 +21,18 @@ const useStyles = makeStyles(theme => ({
  *          if likes is at the bottom of the page, it has to render above the likes 
  *          if likes is at the top of the page, it has to render above the likes 
  */
-const LikesAndComments = ({ postId, likesArr }) => {
+const ShowProfilesThatLikedPost = ({  }) => {
   // init hooks
   const classes = useStyles();
-  const likesCt = likesArr.length;
-  const likesMsg = `${likesCt} like${likesCt !== 1 ? 's' : ''}`;
+  // build
+  const listOfAccounts = [];
 
   return (
-    <div className={`LikesAndComments ${classes}`}>
-      <MuiLink
-        component={Link}
-        color="inherit"
-        variant="subtitle2"
-        onClick={() => console.log('asdfsdf, post arr', postId)}
-      >
-        <ShowProfilesThatLikedPost />
-        {likesMsg}
-      </MuiLink>
+    <div className={`ShowProfilesThatLikedPost ${classes.root}`}>
+      {listOfAccounts}
     </div>
   );
 };
 
 // export
-export default LikesAndComments;
+export default ShowProfilesThatLikedPost;
